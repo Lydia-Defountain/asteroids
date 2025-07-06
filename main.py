9e8494026495b99d6a9251dc5c085f2231eef698
Lydia-Defountain <christinlwright@gmail.com>
2025-07-06 15:17:10 -0400

    Have a moving player!

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
    
 
 
